e9e79339469c01a0d512041f17a0c26e5d728c7a
heewon <hw7666@sogang.ac.kr>
2024-05-12 01:28:29 +0900

    💄 design: 기록 부분 UI 구현

--- src/pages/plogging/PloggingPage.tsx
+++ src/pages/plogging/PloggingPage.tsx
@@ -5,6 +5,7 @@ import styled from "styled-components";
 function PloggingPage() {
   const navigate = useNavigate();
   const [sec, setSec] = useState(0);
+  const [dst, setDst] = useState(0.0);
 
   const calcSec = () => {
     setSec((prev) => ++prev);
@@ -37,8 +38,13 @@ function PloggingPage() {
 
   return (
     <Container>
-      <Timer>{`${calcTime(sec, "h").toString().padStart(2, "0")} : ${calcTime(sec, "m").toString().padStart(2, "0")} : ${(sec % 60).toString().padStart(2, "0")}`}</Timer>
-      <Button onClick={handleStopClick}>종료</Button>
+      <ResultSection>
+        <Timer>{`${calcTime(sec, "h").toString().padStart(2, "0")} : ${calcTime(sec, "m").toString().padStart(2, "0")} : ${(sec % 60).toString().padStart(2, "0")}`}</Timer>
+        <Distance>
+          {dst.toFixed(4)} <Span>km</Span>
+        </Distance>
+        <Button onClick={handleStopClick}>종료</Button>
+      </ResultSection>
     </Container>
   );
 }
@@ -46,11 +52,13 @@ function PloggingPage() {
 export default PloggingPage;
 
 const Container = styled.div`
-  margin-top: 16px;
+  margin-top: 24px;
 `;
 
 const Button = styled.button`
+  width: 138px;
   padding: 8px 0;
+  margin-top: 12px;
   background-color: #ff4444;
   border-radius: 16px;
 
@@ -63,4 +71,25 @@ const Timer = styled.div`
   font-size: 40px;
   font-weight: 900;
   text-align: center;
+`;
+
+const ResultSection = styled.div`
+  height: 175px;
+  border-bottom: 1px solid #d9d9d9;
+
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+  align-items: center;
+`;
+
+const Distance = styled.div`
+  font-size: 24px;
+  font-weight: 900;
+  text-align: center;
+`;
+
+const Span = styled.span`
+  font-size: 16px;
+  color: #cdcdcd;
 `;
