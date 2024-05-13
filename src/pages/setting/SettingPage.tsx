af0bad6459ee2c0b33b3d9ea291bbf033fdade7c
Eunkyo Jeong <eunkyo789@naver.com>
2024-05-13 12:25:41 +0900

    Revert "✨feat: Setting Page 닉네임 받아오기 기능 추가"
    This reverts commit b29e2ec7294ed487fb285871d017dbca3b63888c.

--- src/pages/setting/SettingPage.tsx
+++ src/pages/setting/SettingPage.tsx
@@ -1,64 +1,7 @@
-import React, { useState, ChangeEvent } from "react";
-import styled from "styled-components";
-import { Link } from "react-router-dom";
+import React from "react";
 
-interface SettingPageProps {
-  nickname: string;
-  setNickname: React.Dispatch<React.SetStateAction<string>>;
+function SettingPage() {
+  return <div>SettingPage</div>;
 }
-
-function SettingPage({ nickname }: SettingPageProps) {
-  return (
-    <div>
-      <CloverProfileImage src="/clover-profile.png" alt="Clover Profile" />
-
-      <div style={{ marginTop: "20px" }}>
-        <Label htmlFor="nicknameInput">닉네임</Label>
-        <Input type="text" id="nicknameInput" placeholder="닉네임을 입력하세요." value={nickname} />
-        <Label style={{ color: "#bebebe", fontWeight: "normal", fontSize: "14px" }}>닉네임은 10글자 이하로 설정해주세요.</Label>
-        <Button>저장하기</Button>
-      </div>
-    </div>
-  );
-}
-
-const CloverProfileImage = styled.img`
-  width: auto;
-  height: 100px;
-  margin-top: 50px;
-  margin-left: 50px;
-`;
-
-const Label = styled.label`
-  display: block;
-  margin-top: 10px;
-  margin-left: 30px;
-  color: #000000;
-  font-weight: bold;
-`;
-
-const Input = styled.input`
-  width: 400px;
-  height: 60px; /* 원하는 높이로 조절하세요 */
-  margin-top: 5px;
-  margin-left: 30px;
-  padding: 5px;
-  border: 1px solid #bebebe;
-  background-color: #ffffff;
-  font-size: 16px;
-`;
-
-const Button = styled.button`
-  width: 400px;
-  height: 60px;
-  margin-top: 50px;
-  margin-left: 30px;
-  background-color: #54a300;
-  color: #ffffff;
-  border: none;
-  cursor: pointer;
-  font-weight: bold;
-  font-size: 20px;
-`;
 
 export default SettingPage;
