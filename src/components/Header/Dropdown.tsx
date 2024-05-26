ba76a4485a12dd068daa9e088f8455ffdd05f666
Eunkyo Jeong <eunkyo789@naver.com>
2024-05-26 20:09:40 +0900

    feat: babel dependency 추가

--- src/components/Header/Dropdown.tsx
+++ src/components/Header/Dropdown.tsx
@@ -1,4 +1,45 @@
-import { Link, useNavigate } from "react-router-dom";
+// import { Link, useNavigate } from "react-router-dom";
+// import styled from "styled-components";
+
+// function Dropdown() {
+//   const navigate = useNavigate();
+//   return (
+//     <Box>
+//       <List onClick={() => navigate("/setting")}>계정설정</List>
+//       <List>로그아웃</List>
+//     </Box>
+//   );
+// }
+
+// export default Dropdown;
+
+// const Box = styled.div`
+//   width: 120px;
+
+//   background-color: white;
+//   border-radius: 10px;
+//   box-shadow: 1px 1px 4px 1px rgba(0, 0, 0, 0.15);
+
+//   position: absolute;
+//   top: 50px;
+//   right: 0;
+
+//   overflow: hidden;
+// `;
+
+// const List = styled.div`
+//   width: 100%;
+//   padding: 6px 8px;
+
+//   cursor: pointer;
+
+//   &:hover {
+//     background-color: #dbffb680;
+//     color: #54a300;
+//   }
+// `;
+
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
 function Dropdown() {
@@ -15,24 +56,19 @@ export default Dropdown;
 
 const Box = styled.div`
   width: 120px;
-
   background-color: white;
   border-radius: 10px;
   box-shadow: 1px 1px 4px 1px rgba(0, 0, 0, 0.15);
-
   position: absolute;
   top: 50px;
   right: 0;
-
   overflow: hidden;
 `;
 
 const List = styled.div`
   width: 100%;
   padding: 6px 8px;
-
   cursor: pointer;
-
   &:hover {
     background-color: #dbffb680;
     color: #54a300;
--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,4 +1,87 @@
-/* global kakao */
+// /* global kakao */
+// import { TRASH } from "assets/data/trash";
+// import React, { useEffect } from "react";
+// import styled from "styled-components";
+// import trashMarker from "../../assets/icon/trash-marker.svg";
+
+// declare const window: typeof globalThis & {
+//   kakao: any;
+// };
+
+// interface Props {
+//   curPositionArr: { latitude: number | null; longitude: number | null }[];
+//   mapInfo: { latitude: number; longitude: number; level: number };
+//   setMapInfo: any;
+// }
+
+// function KakaoMap({ curPositionArr, mapInfo, setMapInfo }: Props) {
+//   useEffect(() => {
+//     const curPosition = curPositionArr[curPositionArr.length - 1];
+//     if (window.kakao) {
+//       window.kakao.maps.load(() => {
+//         const container = document.getElementById("map");
+//         const options = {
+//           center: new window.kakao.maps.LatLng(mapInfo.latitude, mapInfo.longitude),
+//           level: mapInfo.level,
+//         };
+//         const map = new window.kakao.maps.Map(container, options);
+//         // map.setDraggable(false); //드래그 금지
+
+//         //쓰레기통 마커
+//         const imageSize = new window.kakao.maps.Size(32, 32); // 마커이미지의 크기입니다
+
+//         for (const trash of TRASH) {
+//           const markerImage = new window.kakao.maps.MarkerImage(trashMarker, imageSize),
+//             markerPosition = new window.kakao.maps.LatLng(trash.latitude, trash.longitude); // 마커가 표시될 위치입니다
+
+//           const marker = new window.kakao.maps.Marker({
+//             position: markerPosition,
+//             image: markerImage,
+//           });
+
+//           marker.setMap(map);
+//         }
+
+//         if (curPosition.latitude && curPosition.longitude) {
+//           // marker (user)
+//           const markerPosition = new window.kakao.maps.LatLng(curPosition.latitude, curPosition.longitude);
+//           const marker = new window.kakao.maps.Marker({
+//             position: markerPosition,
+//           });
+//           marker.setMap(map);
+
+//           // line
+//           const linePath = curPositionArr.map((position) => new window.kakao.maps.LatLng(position.latitude, position.longitude));
+//           const polyline = new window.kakao.maps.Polyline({
+//             path: linePath, // 선을 구성하는 좌표배열 입니다
+//             strokeWeight: 5, // 선의 두께 입니다
+//             strokeColor: "#54a300", // 선의 색깔입니다
+//             strokeOpacity: 0.7, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
+//             strokeStyle: "solid", // 선의 스타일입니다
+//           });
+//           polyline.setMap(map);
+//         }
+
+//         window.kakao.maps.event.addListener(map, "center_changed", function () {
+//           const level = map.getLevel();
+//           const latlng = map.getCenter();
+//           setMapInfo({ level, latitude: latlng.Ma, longitude: latlng.La });
+//         });
+//       });
+//     }
+//   }, [curPositionArr]);
+
+//   return <MapContainer id="map"></MapContainer>;
+// }
+
+// export default KakaoMap;
+
+// export const MemoizedMap = React.memo(KakaoMap);
+
+// const MapContainer = styled.div`
+//   width: 100%;
+//   min-height: calc(100vh - 239px);
+// `;
 import { TRASH } from "assets/data/trash";
 import React, { useEffect } from "react";
 import styled from "styled-components";
@@ -8,10 +91,21 @@ declare const window: typeof globalThis & {
   kakao: any;
 };
 
+interface Position {
+  latitude: number | null;
+  longitude: number | null;
+}
+
+interface MapInfo {
+  latitude: number;
+  longitude: number;
+  level: number;
+}
+
 interface Props {
-  curPositionArr: { latitude: number | null; longitude: number | null }[];
-  mapInfo: { latitude: number; longitude: number; level: number };
-  setMapInfo: any;
+  curPositionArr: Position[];
+  mapInfo: MapInfo;
+  setMapInfo: (info: MapInfo) => void;
 }
 
 function KakaoMap({ curPositionArr, mapInfo, setMapInfo }: Props) {
@@ -25,14 +119,12 @@ function KakaoMap({ curPositionArr, mapInfo, setMapInfo }: Props) {
           level: mapInfo.level,
         };
         const map = new window.kakao.maps.Map(container, options);
-        // map.setDraggable(false); //드래그 금지
 
-        //쓰레기통 마커
-        const imageSize = new window.kakao.maps.Size(32, 32); // 마커이미지의 크기입니다
+        const imageSize = new window.kakao.maps.Size(32, 32);
 
         for (const trash of TRASH) {
-          const markerImage = new window.kakao.maps.MarkerImage(trashMarker, imageSize),
-            markerPosition = new window.kakao.maps.LatLng(trash.latitude, trash.longitude); // 마커가 표시될 위치입니다
+          const markerImage = new window.kakao.maps.MarkerImage(trashMarker, imageSize);
+          const markerPosition = new window.kakao.maps.LatLng(trash.latitude, trash.longitude);
 
           const marker = new window.kakao.maps.Marker({
             position: markerPosition,
@@ -43,21 +135,19 @@ function KakaoMap({ curPositionArr, mapInfo, setMapInfo }: Props) {
         }
 
         if (curPosition.latitude && curPosition.longitude) {
-          // marker (user)
           const markerPosition = new window.kakao.maps.LatLng(curPosition.latitude, curPosition.longitude);
           const marker = new window.kakao.maps.Marker({
             position: markerPosition,
           });
           marker.setMap(map);
 
-          // line
           const linePath = curPositionArr.map((position) => new window.kakao.maps.LatLng(position.latitude, position.longitude));
           const polyline = new window.kakao.maps.Polyline({
-            path: linePath, // 선을 구성하는 좌표배열 입니다
-            strokeWeight: 5, // 선의 두께 입니다
-            strokeColor: "#54a300", // 선의 색깔입니다
-            strokeOpacity: 0.7, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
-            strokeStyle: "solid", // 선의 스타일입니다
+            path: linePath,
+            strokeWeight: 5,
+            strokeColor: "#54a300",
+            strokeOpacity: 0.7,
+            strokeStyle: "solid",
           });
           polyline.setMap(map);
         }
@@ -69,9 +159,9 @@ function KakaoMap({ curPositionArr, mapInfo, setMapInfo }: Props) {
         });
       });
     }
-  }, [curPositionArr]);
+  }, [curPositionArr, mapInfo, setMapInfo]);
 
-  return <MapContainer id="map"></MapContainer>;
+  return <MapContainer id="map" />;
 }
 
 export default KakaoMap;
