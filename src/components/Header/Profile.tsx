5778d741e17c479075b4c868430b09b5c5b9c8a9
heewon <hw7666@sogang.ac.kr>
2024-06-02 21:38:17 +0900

    🐛 fix: 헤더 닉네임 연동 문제 해결

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,27 +1,25 @@
 import { useContext, useEffect, useState } from "react";
 import styled from "styled-components";
 import Dropdown from "./Dropdown";
-import api from "api/axios";
 import { useNavigate } from "react-router-dom";
 import AuthContext from "api/auth-context";
 
 function Profile() {
   const [isLogin, setIsLogin] = useState(false);
-  const {
-    userObj: { nickname },
-  } = useContext(AuthContext);
+  const { userObj, getUser } = useContext(AuthContext);
   const [isOpen, setIsOpen] = useState(false);
   const navigate = useNavigate();
 
   useEffect(() => {
-    if (localStorage.getItem("token")) {
+    if (userObj.nickname || window.localStorage.getItem("token")) {
+      if (!userObj.nickname) getUser();
       setIsLogin(true);
     }
-  }, []);
+  }, [userObj]);
 
   return (
     <Wrapper>
-      <span onClick={() => (isLogin ? setIsOpen((prev) => !prev) : navigate("/login"))}>{isLogin ? `${nickname} 님` : "로그인"}</span>
+      <span onClick={() => (isLogin ? setIsOpen((prev) => !prev) : navigate("/login"))}>{isLogin ? `${userObj.nickname} 님` : "로그인"}</span>
       {isOpen && <Dropdown />}
     </Wrapper>
   );
