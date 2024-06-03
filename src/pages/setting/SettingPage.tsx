56495ea10a7aceee5a05c4b9a807f2d987113ca9
Eunkyo Jeong <eunkyo789@naver.com>
2024-06-03 15:53:06 +0900

    Revert "각 bottomnav 사용 하단 공간 추가"
    This reverts commit 799fae8ae3983ccb2660a59fe9d3d86cb54a0fd4.

--- src/pages/setting/SettingPage.tsx
+++ src/pages/setting/SettingPage.tsx
@@ -81,7 +81,6 @@ function SettingPage() {
           {isLoading ? "저장 중..." : "저장하기"}
         </Button>
       </FormContainer>
-      <Spacer />
       <BottomNav />
     </Container>
   );
@@ -95,10 +94,6 @@ const Container = styled.div`
   padding: 16px;
   justify-content: center;
   align-items: center;
-`;
-
-const Spacer = styled.div`
-  height: 80px; /* 80px spacer div */
 `;
 
 const CloverProfileImage = styled.img`
